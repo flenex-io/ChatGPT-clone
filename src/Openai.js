75ca799f01c0c546fcebde46181e872ee06cf44a
Jagjot Singh <dev.flenex@gmail.com>
2023-11-10 23:03:09 +0530

    Updated Openai.js file

--- src/Openai.js
+++ src/Openai.js
@@ -18,22 +18,3 @@ export async function sendMsgToOpenAI(message) {
 
   return res.data.choices[0].text;
 }
-
-// import OpenAI from 'openai';
-
-// const openai = new OpenAI({
-//     apiKey: '8psZhSNP1kFKNNDonPDQT3BlbkFJyOA7izvCDWIbfAaCxRVL' // This is also the default, can be omitted
-//   });
-
-// export async function sendMsgToOpenAI(message) {
-//   const res = await openai.createCompletion({
-//     model: 'text-davinci-003',
-//     prompt: message,
-//     temperature: 0.7,
-//     max_tokens: 256,
-//     top_p: 1,
-//     frequency_penalty: 0,
-//     presence_penalty: 0
-//   });
-//   return res.data.choices[0].text;
-// }
